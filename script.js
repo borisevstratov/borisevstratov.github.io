451ed6ae14891e2abbca666ae0f316005a0fffc3
Boris Evstratov <borisevstratov@gmail.com>
2025-09-04 17:45:29 +0300

    Updates

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
 	const el = document.getElementById("time");
 	el.innerHTML = new Date().toLocaleString();
 
-	setInterval(function () {
+	setInterval(() => {
 		el.innerHTML = new Date().toLocaleString();
 	}, 1000);
 });
@@ -12,16 +12,10 @@ function copyText(text, message = "") {
 	alert(message + " copied to clipboard 👍");
 }
 
-(function (m, e, t, r, i, k, a) {
-	m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments); };
-	m[i].l = 1 * new Date();
-	for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } }
-	k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a);
-})
-	(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-ym(99108327, "init", {
-	clickmap: true,
-	trackLinks: true,
-	accurateTrackBounce: true
-});
+(() => {
+	var s = document.createElement("script");
+	s.src = "http://localhost:5173/tracker.js";
+	s.setAttribute("data-website", "bevstratov.ru");
+	s.async = true;
+	document.head.appendChild(s);
+})();
